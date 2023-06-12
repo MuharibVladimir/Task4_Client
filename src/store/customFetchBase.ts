344b8fb9eafdf84ba90d88f268a4b9d30f8ac3a9
Vladimir Turomsha <zver010810@gmail.com>
2023-06-12 16:56:08 +0300

    add address

--- src/store/customFetchBase.ts
+++ src/store/customFetchBase.ts
@@ -9,7 +9,7 @@ import { authSlice } from "./slices/AuthSlice";
 import { IToken } from "../models/IToken";
 import { Navigate, useNavigate } from "react-router-dom";
 
-const baseUrl = `${process.env.REACT_APP_SERVER_ENDPOINT}/api/`;
+const baseUrl = `http://vladimirmuharib-001-site1.atempurl.com/api/`;
 
 // Create a new mutex
 const mutex = new Mutex();
